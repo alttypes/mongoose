73077f18d1f5af2b0118c589305b9560f0e2636e
Richard Bateman <taxilian@gmail.com>
2020-10-12 13:57:48 -0600

    An an extra test "just in case" for correctly handling null documents

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 // This just ensures that the type unit tests in the type file are all
 // passing
 
-import {AUT_Tests} from 'mongoose';
+import {AUT_Tests, LeanDocument} from 'mongoose';
 import {assert, UnitTests} from 'advanced-type-tests';
+
+function assert2<T>(v: T): void {}
+
+// Add an extra check for an issue I hit when using mongoose lean documents with strict mode enabled
+assert2<null>(void 0 as unknown as LeanDocument<null>);
 
 assert<UnitTests.Yes>(void 0 as unknown as AUT_Tests.AllTests);
